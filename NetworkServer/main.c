cc725754acc1180ff3953c82ba8bf9ca8be92392
KBryan <kwame@3einteractive.com>
2016-02-26 08:01:40 -0500

    Initial Commit

---
+++ NetworkServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NetworkServer
+//
+//  Created by KBryan on 2016-02-26.
+//  Copyright © 2016 KBryan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
